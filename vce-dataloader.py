46a3c5b6f5c2efd0ff85339841c8b017868bf132
Bhuvanesh Singla <125354611+Bhuvanesh-Singla@users.noreply.github.com>
2024-10-08 12:14:20 +0530

    VCE Dataloader | Added Sampling

--- vce-dataloader.py
+++ vce-dataloader.py
@@ -1,14 +1,15 @@
-# %% [code]
-# %% [code] {"execution":{"iopub.status.busy":"2024-10-07T18:28:41.161242Z","iopub.execute_input":"2024-10-07T18:28:41.161932Z","iopub.status.idle":"2024-10-07T18:28:46.507300Z","shell.execute_reply.started":"2024-10-07T18:28:41.161886Z","shell.execute_reply":"2024-10-07T18:28:46.505764Z"}}
+# %% [code] {"jupyter":{"outputs_hidden":false}}
+
+# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2024-10-08T06:19:36.274243Z","iopub.execute_input":"2024-10-08T06:19:36.274704Z","iopub.status.idle":"2024-10-08T06:19:36.281537Z","shell.execute_reply.started":"2024-10-08T06:19:36.274661Z","shell.execute_reply":"2024-10-08T06:19:36.280111Z"}}
 import torch
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, WeightedRandomSampler
 import matplotlib.pyplot as plt
 import numpy as np
 import torchvision
 from torchvision.utils import make_grid
 
-# %% [code] {"execution":{"iopub.status.busy":"2024-10-07T18:28:46.509612Z","iopub.execute_input":"2024-10-07T18:28:46.510195Z","iopub.status.idle":"2024-10-07T18:28:46.518863Z","shell.execute_reply.started":"2024-10-07T18:28:46.510152Z","shell.execute_reply":"2024-10-07T18:28:46.517432Z"}}
+# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2024-10-08T06:19:36.508239Z","iopub.execute_input":"2024-10-08T06:19:36.508646Z","iopub.status.idle":"2024-10-08T06:19:36.515892Z","shell.execute_reply.started":"2024-10-08T06:19:36.508608Z","shell.execute_reply":"2024-10-08T06:19:36.514490Z"}}
 class BinaryDataset(Dataset):
     def __init__(self, dataset, target_class_idx):
         self.dataset = dataset
@@ -22,7 +23,7 @@ class BinaryDataset(Dataset):
         binary_label = 1 if label == self.target_class_idx else 0
         return image, binary_label
 
-# %% [code] {"execution":{"iopub.status.busy":"2024-10-07T18:40:50.390312Z","iopub.execute_input":"2024-10-07T18:40:50.390746Z","iopub.status.idle":"2024-10-07T18:40:50.398841Z","shell.execute_reply.started":"2024-10-07T18:40:50.390708Z","shell.execute_reply":"2024-10-07T18:40:50.397435Z"}}
+# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2024-10-08T06:31:40.383731Z","iopub.execute_input":"2024-10-08T06:31:40.384214Z","iopub.status.idle":"2024-10-08T06:31:40.395654Z","shell.execute_reply.started":"2024-10-08T06:31:40.384168Z","shell.execute_reply":"2024-10-08T06:31:40.394214Z"}}
 def getBinaryDataLoader(image_size = (224,224), target_class_name = None, path_to_dataset = None, batch_size = 32):
     
     transform = transforms.Compose([
@@ -37,21 +38,34 @@ def getBinaryDataLoader(image_size = (224,224), target_class_name = None, path_t
     target_class_idx = dataset.class_to_idx[target_class]
     
     binary_dataset = BinaryDataset(dataset, target_class_idx)
-    binaryDataLoader = DataLoader(binary_dataset, batch_size=batch_size, shuffle = True)
-    return binaryDataLoader
+    targets = np.array([label for _, label in binary_dataset])
+    class_sample_counts = np.array([(targets == 0).sum(), (targets == 1).sum()])
+    class_sample_counts = torch.tensor(class_sample_counts)
+    class_weights = 1. / class_sample_counts.float()
+    sample_weights = [class_weights[label] for label in targets]
+    sampler = WeightedRandomSampler(weights=sample_weights, num_samples=len(sample_weights), replacement=True)
     
+    
+    binaryDataLoader = DataLoader(binary_dataset, batch_size=batch_size, shuffle = False, sampler = sampler)
+    return binaryDataLoader
 
-# %% [code] {"execution":{"iopub.status.busy":"2024-10-07T18:40:50.603756Z","iopub.execute_input":"2024-10-07T18:40:50.604214Z","iopub.status.idle":"2024-10-07T18:40:50.610961Z","shell.execute_reply.started":"2024-10-07T18:40:50.604170Z","shell.execute_reply":"2024-10-07T18:40:50.609828Z"}}
+# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2024-10-08T06:31:45.502099Z","iopub.execute_input":"2024-10-08T06:31:45.502701Z","iopub.status.idle":"2024-10-08T06:31:45.513883Z","shell.execute_reply.started":"2024-10-08T06:31:45.502643Z","shell.execute_reply":"2024-10-08T06:31:45.512413Z"}}
 def getAllDataLoader(image_size = (224,224), path_to_dataset = None, batch_size = 32):
     transform = transforms.Compose([
         transforms.Resize(image_size),  # Adjust size based on your model
         transforms.ToTensor(),
     ])
     all_classes_dataset = datasets.ImageFolder(root=path_to_dataset, transform=transform)
-    all_classes_dataloader = DataLoader(all_classes_dataset, batch_size=batch_size, shuffle=True)
+    targets = np.array([label for _, label in all_classes_dataset])
+    class_sample_counts = torch.tensor(np.array([(targets == i).sum() for i in range(len(all_classes_dataset.classes))]))
+    class_weights = 1. / class_sample_counts.float()
+    sample_weights = [class_weights[label] for label in targets]
+    sampler = WeightedRandomSampler(weights=sample_weights, num_samples=len(sample_weights), replacement=True)
+
+    all_classes_dataloader = DataLoader(all_classes_dataset, batch_size=batch_size, shuffle=False, sampler = sampler)
     return all_classes_dataloader
 
-# %% [code] {"execution":{"iopub.status.busy":"2024-10-07T18:46:33.321131Z","iopub.execute_input":"2024-10-07T18:46:33.321587Z","iopub.status.idle":"2024-10-07T18:46:33.330131Z","shell.execute_reply.started":"2024-10-07T18:46:33.321544Z","shell.execute_reply":"2024-10-07T18:46:33.329002Z"}}
+# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2024-10-08T06:19:38.350439Z","iopub.execute_input":"2024-10-08T06:19:38.350980Z","iopub.status.idle":"2024-10-08T06:19:38.360735Z","shell.execute_reply.started":"2024-10-08T06:19:38.350906Z","shell.execute_reply":"2024-10-08T06:19:38.359301Z"}}
 def visualize_batch(dataloader, nrow = 8):
     dataiter = iter(dataloader)
     images, labels = next(dataiter)
@@ -69,7 +83,7 @@ def imshow(img):
     plt.imshow(np.transpose(np_img, (1, 2, 0)))  # Convert from CHW to HWC
     plt.show()
 
-# %% [code] {"execution":{"iopub.status.busy":"2024-10-07T18:45:57.683631Z","iopub.execute_input":"2024-10-07T18:45:57.684088Z","iopub.status.idle":"2024-10-07T18:46:10.314162Z","shell.execute_reply.started":"2024-10-07T18:45:57.684047Z","shell.execute_reply":"2024-10-07T18:46:10.313007Z"}}
+# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2024-10-08T06:31:49.600505Z","iopub.execute_input":"2024-10-08T06:31:49.601045Z","iopub.status.idle":"2024-10-08T06:34:27.782252Z","shell.execute_reply.started":"2024-10-08T06:31:49.600993Z","shell.execute_reply":"2024-10-08T06:34:27.781094Z"}}
 if __name__ == '__main__':
     binDL = getBinaryDataLoader(image_size=(224,224),target_class_name="Normal", 
                                 path_to_dataset="/kaggle/input/vce-dataset/training", batch_size= 32)
@@ -77,5 +91,3 @@ if __name__ == '__main__':
     
     allDL = getAllDataLoader(image_size=(224,224), path_to_dataset="/kaggle/input/vce-dataset/training", batch_size=32)
     visualize_batch(allDL)
-
-# %% [code]
