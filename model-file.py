2536d95c8c05d728f857b6c0b0f996429db47f9e
Bhuvanesh Singla <125354611+Bhuvanesh-Singla@users.noreply.github.com>
2024-10-08 23:06:08 +0530

    Kaggle Notebook | Model File | generalized

--- model-file.py
+++ model-file.py
@@ -1,5 +1,14 @@
-# %% [code]
+# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2024-10-08T17:33:53.426057Z","iopub.execute_input":"2024-10-08T17:33:53.426506Z","iopub.status.idle":"2024-10-08T17:33:58.296559Z","shell.execute_reply.started":"2024-10-08T17:33:53.426464Z","shell.execute_reply":"2024-10-08T17:33:58.295346Z"}}
 import torch
-from torchvision import models
+import torchvision.models as models
 
-model = models.resnet50(weights='IMAGENET1K_V1')
+def getModel(model_name):
+    # Get the model dynamically using getattr
+    model_func = getattr(models, model_name.lower())  # Fetch the model constructor by name
+    model = model_func(pretrained=True)  # Load the pretrained model
+    
+    return model
+
+def getList():
+    return dir(models)
+
